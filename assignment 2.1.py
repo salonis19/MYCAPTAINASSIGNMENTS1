7ee7b048663b9691d0745f4990ff98d02127858a
salonis19 <90784749+salonis19@users.noreply.github.com>
2021-10-24 20:19:50 +0530

    Add files via upload

---
+++ assignment 2.1.py
@@ -0,0 +1,20 @@
+
+
+
+
+# Python program to demonstrate working
+# of dictionary copy
+dic = {1:'geeks', 2:'for', 3:'geeks'}
+print('original: ', dic)
+ 
+# Accessing value for key
+print(dic.get(1))
+ 
+# Accessing keys for the dictionary
+print(dic.keys())
+ 
+# Accessing keys for the dictionary
+print(dic.values())
+ 
+# Printing all the items of the Dictionary
+print(dic.items())
